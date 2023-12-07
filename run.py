428e4cd37f0faff947e63b648c66127393d26539
bellicosi-in <73630139+bellicosi-in@users.noreply.github.com>
2023-12-07 21:29:32 +0530

    double recursion

--- run.py
+++ run.py
@@ -3,10 +3,15 @@ def recursion(x):
         # print(x)
     while(x!=2) and x is not None:
         print(x)
+        print("recursion")
         x=x-1
         print(x)
         recursion(x)
         
+        # while x is not None:
+
+        recursion(x)
+        return x
     
 
 recursion(8)
@@ -17,3 +22,13 @@ def sum(x):
     return recursion(x) + recursion(x)
 
 recursion(recursion(8))
+recursion(8)
+
+
+def run(x):
+    print("double recursion")
+    if x is not None:
+        return recursion(x)+ recursion(x*2)
+
+
+run(5)
